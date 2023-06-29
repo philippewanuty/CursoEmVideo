a38c3decdbc4030c76b46216ca25ea0cc116a58a
Philippe Wanuty <66501091+philippewanuty@users.noreply.github.com>
2023-06-29 09:37:19 -0300

    rename

--- AULAS - PYTHON/DESAFIOS/11 A 20/DESAFIO 012.py
+++
@@ -1,8 +0,0 @@
-#D012 FAÇA UM ALGORITMO QUE LEIA O PREÇO DE UM PRODUTO E MOSTRE SEU NOVO PREÇO, COM 5% DE DESCONTO.
-
-p = float(input('Qual o valor antigo do seu produto? '))
-d = p - (0.05*p)
-
-print('O valor do seu produto era {} e com 5% de desconto fica {:.2f}'.format(p, d))
-
-#DESAFIO CONCLUÍDO
--- AULAS - PYTHON/DESAFIOS/11 A 20/DESAFIO 013.py
+++
@@ -1,8 +0,0 @@
-#D013 FAÇA UM ALGORITMO QUE LEIA O SALÁRIO DE UM FUNCIONÁRIO E MOSTRE SEU NOVO SALÁRIO, COM 15% DE AUMENTO.
-
-s = float(input('Qual é o seu salário? '))
-a = s + (0.15 * s)
-
-print('Voce teve um aumento de 15% seu novo salário é de {:.2f}'.format(a))
-
-#DESAFIO CONCLUÍDO
