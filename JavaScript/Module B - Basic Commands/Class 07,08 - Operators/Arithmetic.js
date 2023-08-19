0f3de778619e490d33e581061b5edc1f1af5643f
Philippe Wanuty <philippewanuty@hotmail.com>
2023-08-19 16:31:56 -0300

    Class 07, 08 - Operators

---
+++ JavaScript/Module B - Basic Commands/Class 07,08 - Operators/Arithmetic.js
@@ -0,0 +1,20 @@
+var number1 = 20;
+var number2 = 2;
+
+var sum = number1 + number2;
+var subtraction = number1 - number2;
+var division = number1 / number2;
+var multiplication = number1 * number2;
+var module = number1 % number2
+var exponentiation = number1 ** number2
+var increment = ++number1
+var decrement = --number2
+
+console.log(sum)
+console.log(subtraction)
+console.log(division);
+console.log(multiplication);
+console.log(module);
+console.log(exponentiation);
+console.log(increment)
+console.log(decrement);
