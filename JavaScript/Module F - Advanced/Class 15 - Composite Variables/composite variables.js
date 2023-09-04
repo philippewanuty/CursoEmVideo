32410f5226074b385c99e29685b1e67ca6970821
Philippe Wanuty <philippewanuty@hotmail.com>
2023-09-04 17:43:28 -0300

    Class 15 - Composite Variables

---
+++ JavaScript/Module F - Advanced/Class 15 - Composite Variables/composite variables.js
@@ -0,0 +1,16 @@
+let num = [4, 2, 1, 9, 3, 6, 8, 7, ];
+
+console.log(num);
+console.log(num[3]);
+
+//push
+
+console.log(num);
+
+//sort
+num.sort();
+
+console.log(num);
+
+// index
+console.log(num.indexOf(9))
